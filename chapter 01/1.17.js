cc13383a86bedd463382c474b0d55115a2f970bf
Alan Chan <alanchanzm@outlook.com>
2019-01-23 21:21:17 +0800

    feat(1.17): multiplication

---
+++ chapter 01/1.17.js
@@ -0,0 +1,26 @@
+const { is_even } = require('../util/index');
+
+const double = x => x + x;
+const halve = x => x / 2;
+
+function multiplication(a, b) {
+  if (a === 0 || b === 0) return 0;
+  if (b === 1) return a;
+  if (a === 1) return b;
+  if (is_even(b)) return double(multiplication(a, halve(b)));
+  return a + multiplication(a, b - 1);
+}
+
+function test() {
+  [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100].forEach((ans, n) => {
+    if (multiplication(n, n) !== ans) {
+      throw new Error(
+        `multiplication(${n}, ${n}) should be ${ans}, 
+        not ${multiplication(n, n)}`
+      );
+    }
+  });
+  console.log('tests pass');
+}
+
+test();
