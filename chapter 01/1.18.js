a5da81774c658665bb12483be271287e7f6fe5ff
Alan Chan <alanchanzm@outlook.com>
2019-01-24 10:07:24 +0800

    feat(1.18): multiplication

---
+++ chapter 01/1.18.js
@@ -0,0 +1,16 @@
+const { is_even } = require('../util/index');
+const { double, halve, test } = require('./1.17');
+
+function multiplication(a, b) {
+  return multiplication_iter();
+
+  function multiplication_iter(product = 0, x = a, y = b) {
+    if (x === 0 || y === 0) return product;
+    if (x === 1) return y + product;
+    if (y === 1) return x + product;
+    if (is_even(y)) return multiplication_iter(product, double(x), halve(y));
+    return multiplication_iter(product + x, x, y - 1);
+  }
+}
+
+test(multiplication);
