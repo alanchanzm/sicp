b4522a2694c2c926085764414e07068fef8b5eff
Alan Chan <alanchanzm@outlook.com>
2019-01-27 21:47:09 +0800

    docs(2.04):

--- src/ch02/2.04.js
+++ src/ch02/2.04.js
@@ -1,13 +1,19 @@
+// cons 返回一个函数，接收一个参数 m
+// m 返回 m 执行的结果
 function cons(x, y) {
-  return m => m(x, y);
+  return function z(m) {
+    return m(x, y);
+  };
+}
+
+// cdr 接收一个参数 函数-z，返回 z 执行的结果
+function cdr(z) {
+  // (x, y) => y 就是函数 m
+  return z((x, y) => y);
 }
 
 function car(z) {
-  return z(p => p);
-}
-
-function cdr(z) {
-  return z((_, q) => q);
+  return z(x => x);
 }
 
 export { cons, car, cdr };
