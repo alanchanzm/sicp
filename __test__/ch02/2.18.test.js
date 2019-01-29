0e53dfe9c6a9b0a289a66993ae0cd424309d2b20
Alan Chan <alanchanzm@outlook.com>
2019-01-29 09:42:37 +0800

    test(2.18):

--- __test__/ch02/2.18.test.js
+++ __test__/ch02/2.18.test.js
@@ -1,13 +1,6 @@
-import { list, car, cdr } from '../../util/index';
+import { list, value_of } from '../../util/index';
 import fn from '../../src/ch02/2.18';
 
-test(`${fn.name} --- list(23,72,149,34)`, () => {
-  let reversed_list = fn(list(23, 72, 149, 34));
-  expect(car(reversed_list)).toBe(34);
-  reversed_list = cdr(reversed_list);
-  expect(car(reversed_list)).toBe(149);
-  reversed_list = cdr(reversed_list);
-  expect(car(reversed_list)).toBe(72);
-  reversed_list = cdr(reversed_list);
-  expect(car(reversed_list)).toBe(23);
+test(`${fn.name} --- list(23,72,149,34) --- [34,149,72,23]`, () => {
+  expect(value_of(fn(list(23, 72, 149, 34)))).toEqual([34, 149, 72, 23]);
 });
